73d394fca4872f9f1fbdc51f5fdb42f312972fbe
desko27 <desko27@gmail.com>
2018-08-19 13:25:56 +0200

    Add react with parcel

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// eslint-disable-next-line
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Super Smash Bros Tier List</h1>
+      </div>
+    );
+  }
+}
