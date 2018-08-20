cb13aaaab97a89170b6e621b2ad5e9ee5d85e6fc
desko27 <desko27@gmail.com>
2018-08-20 16:34:03 +0200

    Add pending tests for Character

--- src/logic/character.test.js
+++ src/logic/character.test.js
@@ -1,6 +1,10 @@
 describe('Character', () => {
   describe('has the following initial properties:', () => {
-    it('name');
-    it('tier');
+    it('allowedTiers');
+  });
+  describe('throws error if argument', () => {
+    it('name is not a string');
+    it('tier is not a string from allowedTiers');
+    it('image is not a link');
   });
 });
