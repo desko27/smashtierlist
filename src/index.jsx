fafcde9410a0f0b16cfbe09219858810047850ee
desko27 <desko27@gmail.com>
2018-08-26 19:35:25 +0200

    Migrate to webpack

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './redux/app/reducer';
 import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app'),
+);
+
+registerServiceWorker();
