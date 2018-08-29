b6066d3d8a5fa30666f84a278a1d287d1424330b
desko27 <desko27@gmail.com>
2018-08-29 16:32:05 +0200

    Convert rosterGroupedByTier into a curried selector, add test for visible roster tier groups

--- src/containers/Game.jsx
+++ src/containers/Game.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-import { rosterGroupedByTierSelector } from '../redux/game/selectors';
+import { visibleRosterGroupedByTierSelector } from '../redux/game/selectors';
 import { currentGameSelector } from '../redux/app/selectors';
 
 import Filter from '../components/Filter';
@@ -18,7 +18,7 @@ class Game extends React.Component {
     return (
       <div>
         <Filter onChange={this.onFilterChange} />
-        <Roster charactersByTier={rosterGroupedByTierSelector(currentState)} />
+        <Roster charactersByTier={visibleRosterGroupedByTierSelector(currentState)} />
       </div>
     );
   }
--- src/redux/game/selectors.test.js
+++ src/redux/game/selectors.test.js
@@ -1,20 +1,20 @@
-import { rosterGroupedByTierSelector } from './selectors';
+import { allRosterGroupedByTierSelector, visibleRosterGroupedByTierSelector } from './selectors';
 
 const validGameState = {
   roster: [
-    { name: 'Pikachu', tier: 'S' },
-    { name: 'Kirby', tier: 'S' },
-    { name: 'Captain Falcon', tier: 'A' },
-    { name: 'Fox', tier: 'A' },
-    { name: 'Yoshi', tier: 'A' },
-    { name: 'Jigglypuff', tier: 'B' },
+    { name: 'Pikachu', tier: 'S', visible: true },
+    { name: 'Kirby', tier: 'S', visible: true },
+    { name: 'Captain Falcon', tier: 'A', visible: false },
+    { name: 'Fox', tier: 'A', visible: false },
+    { name: 'Yoshi', tier: 'A', visible: true },
+    { name: 'Jigglypuff', tier: 'B', visible: false },
   ],
 };
 
 describe('game selectors', () => {
   describe('has rosterGroupedByTierSelector that', () => {
     it('returns an array of tier groups correctly distributed', () => {
-      const tierGroups = rosterGroupedByTierSelector(validGameState);
+      const tierGroups = allRosterGroupedByTierSelector(validGameState);
       expect(tierGroups).to.have.lengthOf(3);
       expect(tierGroups[0].tier).to.equal('S');
       expect(tierGroups[0].characters).to.have.lengthOf(2);
@@ -22,6 +22,16 @@ describe('game selectors', () => {
       expect(tierGroups[1].characters).to.have.lengthOf(3);
       expect(tierGroups[2].tier).to.equal('B');
       expect(tierGroups[2].characters).to.have.lengthOf(1);
+    });
+  });
+  describe('has visibleRosterGroupedByTierSelector that', () => {
+    it('returns an array of tier groups with only visible characters', () => {
+      const tierGroups = visibleRosterGroupedByTierSelector(validGameState);
+      expect(tierGroups).to.have.lengthOf(2);
+      expect(tierGroups[0].tier).to.equal('S');
+      expect(tierGroups[0].characters).to.have.lengthOf(2);
+      expect(tierGroups[1].tier).to.equal('A');
+      expect(tierGroups[1].characters).to.have.lengthOf(1);
     });
   });
 });
