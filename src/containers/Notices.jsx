cb84423f6d5cd8a98c55e2a19092b6671c76e673
desko27 <desko27@gmail.com>
2019-01-23 20:44:02 +0100

    Add animation for enter/exit Notices

--- src/containers/Notices.jsx
+++ src/containers/Notices.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import posed, { PoseGroup } from 'react-pose';
 import storage from 'store';
 
 import { currentGameSelector } from '../redux/app/selectors';
@@ -8,6 +9,21 @@ import { currentGameSelector } from '../redux/app/selectors';
 import Notice from '../components/Notice';
 
 import { Wrapper } from './Notices.styles';
+
+const PoseItem = posed.div({
+  enter: {
+    opacity: 1,
+    transform: 'translateX(0vw)',
+    height: 'auto',
+    marginBottom: '30px',
+  },
+  exit: {
+    opacity: 0,
+    transform: 'translateX(50vw)',
+    height: '0px',
+    marginBottom: '0px',
+  },
+});
 
 const getNoticeKey = ({ slug, id }) => `${slug}-${id}`;
 
@@ -24,24 +40,27 @@ class Notices extends Component {
 
     return (
       <Wrapper>
-        {
-          currentGame.notices
-            .filter(({ id }) => (
-              isServer || showAll || !storage.get(getNoticeKey({ slug: currentGame.slug, id }))
-            ))
-            .map(({ id, message }) => (
-              <Notice
-                key={id}
-                onClose={() => this.handleCloseNotice(id)}
-                hideCloseButton={showAll}
-              >
-                {
-                  // message with target="_blank" attribute at any link
-                  message.replace(/<a /g, '<a target="_blank" ')
-                }
-              </Notice>
-            ))
-        }
+        <PoseGroup flipMove={false}>
+          {
+            currentGame.notices
+              .filter(({ id }) => (
+                isServer || showAll || !storage.get(getNoticeKey({ slug: currentGame.slug, id }))
+              ))
+              .map(({ id, message }) => (
+                <PoseItem key={id}>
+                  <Notice
+                    onClose={() => this.handleCloseNotice(id)}
+                    hideCloseButton={showAll}
+                  >
+                    {
+                      // message with target="_blank" attribute at any link
+                      message.replace(/<a /g, '<a target="_blank" ')
+                    }
+                  </Notice>
+                </PoseItem>
+              ))
+          }
+        </PoseGroup>
       </Wrapper>
     );
   }
