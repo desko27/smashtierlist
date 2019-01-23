2f6e045a73d750062c314cea6a4626487c37ed71
desko27 <desko27@gmail.com>
2019-01-23 20:09:15 +0100

    Use react-pose to animate enter/exit characters filtering

--- src/components/Roster/TierGroup.jsx
+++ src/components/Roster/TierGroup.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import posed, { PoseGroup } from 'react-pose';
+
 import Character from './Character';
 
 import { Wrapper, Header, Main } from './TierGroup.styles';
+
+const PoseItem = posed.div({
+  enter: { opacity: 1 },
+  exit: { opacity: 0 },
+});
 
 const TierGroup = ({ gameSlug, tier, characters }) => (
   <Wrapper>
@@ -10,7 +17,13 @@ const TierGroup = ({ gameSlug, tier, characters }) => (
       {tier.name}
     </Header>
     <Main>
-      {characters.map(c => <Character key={c.id} gameSlug={gameSlug} {...c} />)}
+      <PoseGroup>
+        {characters.map(c => (
+          <PoseItem key={c.id}>
+            <Character gameSlug={gameSlug} {...c} />
+          </PoseItem>
+        ))}
+      </PoseGroup>
     </Main>
   </Wrapper>
 );
