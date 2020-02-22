3016f7c790ff1fccb1ea68109f89016c7c464337
desko27 <desko27@gmail.com>
2020-02-22 12:28:19 +0100

    Add theme and viewport meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
+import Head from 'next/head'
+
 import '../global.css'
 
 export default function CustomApp ({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=0.9, user-scalable=no' />
+        <meta name='theme-color' content='#000000' />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
