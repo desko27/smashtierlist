9059348a326a55c2c4f251668530333c3454a498
desko27 <desko27@gmail.com>
2020-02-22 18:11:18 +0100

    Place getInitialProps promises in a performant order

--- pages/index.js
+++ pages/index.js
@@ -11,11 +11,23 @@ const Home = ({ gameData, nextGameSlug, prevGameSlug }) => {
 }
 
 Home.getInitialProps = async ({ domain }) => {
-  const getTierlistUseCase = await domain.get('get_tierlist_use_case')
-  const gameData = await getTierlistUseCase.execute(GAME_SLUG)
-  const getNextGameSlugUseCase = await domain.get('get_next_game_slug_use_case')
-  const nextGameSlug = await getNextGameSlugUseCase.execute(GAME_SLUG, 'next')
-  const prevGameSlug = await getNextGameSlugUseCase.execute(GAME_SLUG, 'prev')
+  const fetchGameData = async () => {
+    const getTierlistUseCase = await domain.get('get_tierlist_use_case')
+    return getTierlistUseCase.execute(GAME_SLUG)
+  }
+  const fetchNextGameSlugs = async () => {
+    const getNextGameSlugUseCase = await domain.get('get_next_game_slug_use_case')
+    return Promise.all([
+      getNextGameSlugUseCase.execute(GAME_SLUG, 'next'),
+      getNextGameSlugUseCase.execute(GAME_SLUG, 'prev')
+    ])
+  }
+
+  const [gameData, [nextGameSlug, prevGameSlug]] = await Promise.all([
+    fetchGameData(),
+    fetchNextGameSlugs()
+  ])
+
   return { gameData, nextGameSlug, prevGameSlug }
 }
 
