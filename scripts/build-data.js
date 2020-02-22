bb1c6e47487fbb68862b77cb9fb70d5dfdfa97db
desko27 <desko27@gmail.com>
2020-02-22 17:36:27 +0100

    Add game slugs list file output to the data build script

--- scripts/build-data.js
+++ scripts/build-data.js
@@ -7,11 +7,15 @@ const gameJsons = games.map(game => ({ slug: game.slug, json: JSON.stringify(gam
 
 gameJsons.forEach(({ slug, json }) => {
   const fileContents = `export default JSON.parse('${json}')`
-
   fs.writeFile(`./data-build/${slug}.js`, fileContents, 'utf8', function (err) {
-    if (err) {
-      throw new Error(`❌ Error while writing '${slug}' JSON to file.`)
-    }
+    if (err) throw new Error(`❌ Error while writing '${slug}' JSON to file.`)
     console.log(`✅ Success writing '${slug}' JSON to file.`)
   })
+})
+
+const gameSlugs = gameJsons.map(({ slug }) => slug)
+const fileContents = `export default JSON.parse('${JSON.stringify(gameSlugs)}')`
+fs.writeFile('./data-build/index.js', fileContents, 'utf8', function (err) {
+  if (err) throw new Error('❌ Error while writing all game slugs to file.')
+  console.log('✅ Success writing all game slugs to file.')
 })
