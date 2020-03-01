f7acbf855c6f00ea67a01d4826c5a8f944b701d6
desko27 <desko27@gmail.com>
2020-03-01 16:54:27 +0100

    Add analytics

---
+++ pages/_document.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import Document, { Head, Main, NextScript } from 'next/document'
+
+import { GA_TRACKING_ID } from '../lib/gtag'
+
+export default class extends Document {
+  render () {
+    return (
+      <html>
+        <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <link rel='dns-prefetch' href='//www.googletagmanager.com' />
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('set', 'anonymizeIp', true);
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
